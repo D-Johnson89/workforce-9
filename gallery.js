421990ec69c39c04e79eaef747db4bbe26ca13fd
Dustin Johnson <d.l.johnson3009@gmail.com>
2022-04-08 11:01:31 -0400

    galler slideshow fixed

--- gallery.js
+++ gallery.js
@@ -1,23 +1,31 @@
 const slideshowImages = document
 	.querySelectorAll(".slider .slide")
 	.forEach((slide) => {
-		slide.style.opacity = 1;
+		slide.style.opacity = 0;
 	});
 
-const nextImgDelay = 2500;
-let curImgCount = 0;
+const sliders = document.querySelectorAll(".slider");
+const sliderSlides = [...sliders].map((slider) => {
+	return slider.querySelectorAll(".slide");
+});
 
-//slideshowImages[curImgCount].style.opacity = 1;
+const nextImgDelay = 2500;
+const slidesPagination = sliderSlides.reduce((previous, _, currentIndex) => {
+	return { ...previous, [currentIndex]: { current: 0, previous: -1 } };
+}, {});
 
 setInterval(nextImage, nextImgDelay);
 
 function nextImage() {
-	const sliders = document.querySelectorAll(".slider");
-	for (const slider of sliders) {
-		const slideshowImages = slider.querySelectorAll(".slide");
-		slideshowImages[curImgCount].style.opacity = 0;
-		curImgCount = (curImgCount + 1) % slideshowImages.length;
-
-		slideshowImages[curImgCount].style.opacity = 1;
-	}
+	sliderSlides.forEach((slides, index) => {
+		const currentShownSlide = slidesPagination[index].current;
+		const previousShownSlide = slidesPagination[index].previous;
+		slides[currentShownSlide].style.opacity = 1;
+		if (previousShownSlide !== -1) {
+			slides[previousShownSlide].style.opacity = 0;
+		}
+		slidesPagination[index].previous = currentShownSlide;
+		slidesPagination[index].current =
+			(currentShownSlide + 1) % slides.length;
+	});
 }
